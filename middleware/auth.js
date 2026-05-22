30d1e0a958971913f605d976d1ff94ddacf94ece
Munem223 <munemshahriar223@gmail.com>
2026-05-22 15:41:07 +0600

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,17 +9,6 @@ const protect = (req, res, next) => {
     token = req.cookies.token;
   }
 
-  if (!token) {
-    return res.status(401).json({ message: 'Not authorized, no token provided' });
-  }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(401).json({ message: 'Invalid token' });
-  }
-};
 
 module.exports = protect;
